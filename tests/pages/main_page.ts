fb34f8ab29ccb1d5a3c63094f73f8a7acc7dae8b
Aline Lisboa <lisboalien@gmail.com>
2024-07-04 22:34:44 -0300

    Completed challenge v2

--- tests/pages/main_page.ts
+++ tests/pages/main_page.ts
@@ -1,93 +1,149 @@
-import { type Locator, type Page, expect } from '@playwright/test';
+import { type Locator, type Page, expect } from "@playwright/test";
 
 export class MainPage {
-    readonly page: Page;
-    readonly startButton: Locator;
-    readonly companyName: Locator;
-    readonly address: Locator;
-    readonly ein: Locator;
-    readonly sector: Locator;
-    readonly automationTool: Locator;
-    readonly annualSaving: Locator;
-    readonly date: Locator;
-    readonly submitButton: Locator;
-    readonly resetButton: Locator;
-    readonly signInButton: Locator;
-    readonly orLoginButton: Locator;
-    readonly email: Locator;
-    readonly password: Locator;
-    readonly loginButton: Locator;
+  readonly page: Page;
+  readonly mainSelector: Locator;
+  readonly startButton: Locator;
+  readonly companyName: Locator;
+  readonly address: Locator;
+  readonly einInput: Locator;
+  readonly sector: Locator;
+  readonly automationTool: Locator;
+  readonly annualSaving: Locator;
+  readonly date: Locator;
+  readonly submitButton: Locator;
+  readonly resetButton: Locator;
+  readonly signInButton: Locator;
+  readonly orLoginButton: Locator;
+  readonly email: Locator;
+  readonly password: Locator;
+  readonly loginButton: Locator;
+  readonly captchaButton: Locator;
+  readonly captchaTitle: Locator;
+  readonly inputLocator: String =
+    '.Group:not([style*="display: none"]) .Group:not(:has(.Group)):has(.Text:has-text(":input:")) .Input';
 
-    constructor(page: Page) {
-        this.page = page;
-        this.startButton = page.getByRole('button', { name: 'Start' });
-        this.companyName = page.locator('.bubble-r-line:has(>.bubble-r-box>.Text:has-text("Company Name")) .Input').last() 
-        this.address = page.locator('div:contains("Address") + input');
-        this.ein = page.locator('div:contains("EIN") + input');
-        this.sector = page.locator('div:contains("Sector") + input');
-        this.automationTool = page.locator('div:contains("Automation Tool") + input');
-        this.annualSaving = page.locator('div:contains("Annual Saving") + input');
-        this.date = page.locator('div:contains("Date") + input');
-        this.submitButton = page.getByRole('button', { name: 'Submit' });
-        this.resetButton = page.getByRole('button', { name: 'Reset' });
-        this.signInButton = page.getByRole('button', { name: 'SIGN UP OR LOGIN' });
-        this.orLoginButton = page.getByRole('button', { name: 'OR LOGIN', exact: true });
-        this.email = page.getByRole('textbox', { name: 'Email' });
-        this.password = page.getByRole('textbox', { name: 'Password' });
-        this.loginButton = page.getByRole('button', { name: 'LOG IN' });
-    }
+  constructor(page: Page) {
+    this.page = page;
+    this.startButton = page.getByRole("button", { name: "Start" });
+    this.mainSelector = page.locator(
+      'css=.Group:not([style*="display: none"]) .Group:not(:has(.Group)):has(.Text:has-text("Company Name")) .Input'
+    );
+    this.companyName = page.locator(
+      'css=.Group:not([style*="display: none"]) .Group:not(:has(.Group)):has(.Text:has-text("Company Name")) input'
+    );
+    this.address = page.locator(
+      this.inputLocator.replace(":input:", "Address")
+    );
+    this.einInput = page.locator(this.inputLocator.replace(":input:", "EIN"));
+    this.sector = page.locator(this.inputLocator.replace(":input:", "Sector"));
+    this.automationTool = page.locator(
+      this.inputLocator.replace(":input:", "Automation Tool")
+    );
+    this.annualSaving = page.locator(
+      this.inputLocator.replace(":input:", "Annual Saving")
+    );
+    this.date = page.locator(this.inputLocator.replace(":input:", "Date"));
+    this.submitButton = page.getByRole("button", { name: "Submit" });
+    this.resetButton = page.getByRole("button", { name: "Reset" });
+    this.signInButton = page.getByRole("button", { name: "SIGN UP OR LOGIN" });
+    this.orLoginButton = page.getByRole("button", {
+      name: "OR LOGIN",
+      exact: true,
+    });
+    this.email = page.getByRole("textbox", { name: "Email" });
+    this.password = page.getByRole("textbox", { name: "Password" });
+    this.loginButton = page.getByRole("button", { name: "LOG IN" });
+    this.captchaTitle = page.getByText(
+      "Get through this reCAPTCHA to continue"
+    );
+    this.captchaButton = page.getByRole("button", { name: "presentation" });
+  }
 
-    async clickStartButton() {
-        await this.startButton.click();
-    }
+  async clickStartButton() {
+    await this.startButton.click();
+  }
 
-    async fillCompanyName(companyName: string) {
-        await this.companyName.fill(companyName);
-    }
+  async fillCompanyName(companyName: string) {
+    await this.captchaHandler()
+    await this.companyName.fill(companyName, { timeout: 10000 });
+  }
 
-    async fillAddress(address: string) {
-        await this.address.fill(address);
-    }
+  async fillAddress(address: string) {
+    await this.captchaHandler()
+    await this.address.fill(address);
+  }
 
-    async fillEIN(ein: string) {
-        await this.ein.fill(ein);
-    }
+  async fillEIN(ein: string) {
+    await this.captchaHandler()
+    await this.einInput.fill(ein);
+  }
 
-    async fillSector(sector: string) {
-        await this.sector.fill(sector);
-    }
+  async fillSector(sector: string) {
+    await this.captchaHandler()
+    await this.sector.fill(sector);
+  }
 
-    async fillAutomationTool(automationTool: string) {
-        await this.automationTool.fill(automationTool);
-    }
+  async fillAutomationTool(automationTool: string) {
+    await this.captchaHandler()
+    await this.automationTool.fill(automationTool);
+  }
 
-    async fillAnnualSaving(annualSaving: string) {
-        await this.annualSaving.fill(annualSaving);
-    }
+  async fillAnnualSaving(annualSaving: string) {
+    await this.captchaHandler()
+    await this.annualSaving.fill(annualSaving);
+  }
 
-    async fillDate(date: string) {
-        await this.date.fill(date);
-    }
+  async fillDate(date: string) {
+    await this.captchaHandler()
+    await this.date.fill(date);
+  }
 
-    async clickSubmitButton() {
-        await this.submitButton.click();
-    }
+  async clickSubmitButton() {
+    await this.captchaHandler()
+    await this.submitButton.click();
+  }
 
-    async clickResetButton() {
-        await this.resetButton.click();
-    }
+  async clickResetButton() {
+    await this.resetButton.click();
+  }
 
-    async clickSignInButton() {
-        await this.signInButton.click();
-    }
+  async clickSignInButton() {
+    await this.signInButton.click();
+  }
 
-    async login(email: string, password: string) {
-        await this.signInButton.click();
-        await this.orLoginButton.click();
-        await this.email.fill(email);
-        await this.password.fill(password);
-        await this.loginButton.click();
+  async login(email: string, password: string) {
+    await this.signInButton.click();
+    await this.orLoginButton.click();
+    await this.email.fill(email);
+    await this.password.fill(password);
+    await this.loginButton.click();
+  }
+
+  async fillForm(
+    companyName: string,
+    address: string,
+    ein: string,
+    sector: string,
+    automationTool: string,
+    annualSaving: string,
+    date: string
+  ) {
+    await this.fillCompanyName(companyName);
+    await this.fillAddress(address);
+    await this.fillEIN(ein);
+    await this.fillSector(sector);
+    await this.fillAutomationTool(automationTool);
+    await this.fillAnnualSaving(annualSaving);
+    await this.fillDate(date);
+  }
+
+  async captchaHandler() {
+    if (await this.captchaTitle.isVisible()) {
+      await this.captchaButton.click();
+      await expect(this.captchaTitle).not.toBeVisible();
     }
+  }
 }
 
 export default MainPage;
--- tests/specs/challenge.spec.ts
+++ tests/specs/challenge.spec.ts
@@ -1,35 +1,59 @@
-import fs from 'fs';
-import path from 'path';
-import { test, expect } from '@playwright/test';
-import { parse } from 'csv-parse/sync';
-import { MainPage } from '../pages/main_page';
+import fs from "fs";
+import path from "path";
+import { test, expect } from "@playwright/test";
+import { parse } from "csv-parse/sync";
+import { MainPage } from "../pages/main_page";
 
-const email = process.env.EMAIL ?? '';
-const password = process.env.PASSWORD ?? '';
-
-const records = parse(fs.readFileSync(path.join(__dirname, '../data_driven_tests/challenge.csv')), {
-  columns: true,
-  skip_empty_lines: true
-});
+const email = process.env.EMAIL ?? "";
+const password = process.env.PASSWORD ?? "";
 
 let mainPage: MainPage;
 
-test.describe('Challenge', () => {
+test.describe("Challenge", () => {
   test.beforeEach(async ({ page }) => {
     mainPage = new MainPage(page);
-    await page.goto('/');
+    await page.goto("/");
 
     await mainPage.login(email, password);
   });
 
   //records.forEach((record, index) => {
   test(`TC01 - fill the form`, async ({ page }) => {
-    const index = 0;
-    if (index === 0) {
-      await mainPage.clickStartButton();
-    }
-    await mainPage.fillCompanyName('111');
-  });
-  //});
-});
+    const headers = [
+      "ein",
+      "company_name",
+      "sector",
+      "company_address",
+      "automation_tool",
+      "annual_automation_saving",
+      "date_of_first_project",
+    ];
+    const records = parse(
+      fs.readFileSync(
+        path.join(__dirname, "../data_driven_tests/challenge.csv")
+      ),
+      {
+        delimiter: ";",
+        columns: headers,
+        skip_empty_lines: true,
+      }
+    );
 
+    await mainPage.clickStartButton();
+
+    await expect(mainPage.resetButton).toBeVisible();
+    await expect(mainPage.submitButton).toBeVisible();
+
+    for (let i = 1; i < records.length; i++) {
+      await mainPage.fillCompanyName(records[i].company_name);
+      await mainPage.fillEIN(records[i].ein);
+      await mainPage.fillSector(records[i].sector);
+      await mainPage.fillAutomationTool(records[i].automation_tool);
+      await mainPage.fillAnnualSaving(records[i].annual_automation_saving);
+      await mainPage.fillAddress(records[i].company_address);
+      await mainPage.fillDate(records[i].date_of_first_project);
+
+      await mainPage.clickSubmitButton();
+    }
+  });
+});
